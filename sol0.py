188c8d06613ab82a99b89ad5ab991f15647672e6
eecs388 <eecs388@eecs388.escher.lan>
2022-03-28 02:24:23 +0000

    Automatic commit by test script for alexawan rwaheed

--- sol0.py
+++ sol0.py
@@ -1,2 +1,3 @@
 import sys
-sys.stdout.buffer.write(b"\x72\x77\x61\x68\x65\x65\x64")
+#                           a   l   e   x   a   w   a   n   \0      A   +   \0
+sys.stdout.buffer.write(b"\x61\x6c\x65\x78\x61\x77\x61\x6d\x00\x40\x41\x2b\x00")
